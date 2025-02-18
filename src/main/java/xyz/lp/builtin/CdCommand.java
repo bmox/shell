43b550af22f4a851be7230bb80ac0673bcb11919
ruifeng <ruifeng.yue@outlook.com>
2025-02-18 22:05:18 +0800

    The cd builtin: Relative paths

--- src/main/java/xyz/lp/builtin/CdCommand.java
+++ src/main/java/xyz/lp/builtin/CdCommand.java
@@ -1,6 +1,7 @@
 package xyz.lp.builtin;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 import xyz.lp.Command;
 import xyz.lp.Context;
@@ -14,14 +15,32 @@ public class CdCommand implements Command {
 
     @Override
     public Result execute() {
-        File targetDir = new File(arg);
-        if (!targetDir.exists()) {
-            System.out.println("cd: no such file or directory: " + getArgs()[0]);
-        } else if (!targetDir.isDirectory()) {
-            System.out.println("cd: not a directory: " + getArgs()[0]);
+        File targetDir = new File("/");
+        File inputDir = new File(arg);
+        String[] splittedPath;
+        if (inputDir.isAbsolute()) {
+            splittedPath = inputDir.getAbsolutePath().split(Pattern.quote(File.separator));
         } else {
-            Context.getInstance().setCurrentPath(targetDir.getAbsolutePath());
+            splittedPath = new File(Context.getInstance().getCurrentPath() + File.separator + arg).getAbsolutePath().split(Pattern.quote(File.separator));
         }
+        for (int i = 1; i < splittedPath.length; i++) {
+            String path = splittedPath[i];
+            if (".".equals(path)) {
+                continue;
+            } else if ("..".equals(path)) {
+                targetDir = targetDir.getParentFile() == null ? targetDir : targetDir.getParentFile();
+            } else {
+                targetDir = new File(targetDir.getAbsoluteFile() + File.separator + path);
+            }
+            if (!targetDir.exists()) {
+                System.out.println("cd: no such file or directory: " + getArgs()[0]);
+                return Result.success();
+            } else if (!targetDir.isDirectory()) {
+                System.out.println("cd: not a directory: " + getArgs()[0]);
+                return Result.success();
+            }
+        }
+        Context.getInstance().setCurrentPath(targetDir.getAbsolutePath());
         return Result.success();
     }
 
